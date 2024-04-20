d81c600818bda9a3adbead9c17f35ac66db22d23
g97iulio1609 <giulio97.leone@gmail.com>
2024-04-20 16:59:37 +0200

    Update maxrmdashboard.dart

--- lib/maxrmdashboard.dart
+++ lib/maxrmdashboard.dart
@@ -1,4 +1,3 @@
-import 'dart:js';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart';
 import 'package:rxdart/rxdart.dart';
 import 'exerciseManager/exercise_model.dart';
 import 'exerciseManager/exercises_services.dart';
