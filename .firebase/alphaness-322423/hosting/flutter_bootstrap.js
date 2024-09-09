ac46d739363c72de14b2635dccd9761f04a97b3b
g97iulio1609 <giulio97.leone@gmail.com>
2024-09-09 17:34:03 +0200

    bugfix

--- .firebase/alphaness-322423/hosting/flutter_bootstrap.js
+++ .firebase/alphaness-322423/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2193106171"
+    serviceWorkerVersion: "3979142960"
   }
 });
