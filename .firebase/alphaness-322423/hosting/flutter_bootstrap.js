9c963f4033375c96ffce6f4c86de4b9e5e613815
g97iulio1609 <giulio97.leone@gmail.com>
2024-12-08 23:31:12 +0100

    new Logo/icon

--- .firebase/alphaness-322423/hosting/flutter_bootstrap.js
+++ .firebase/alphaness-322423/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3414367158"
+    serviceWorkerVersion: "1434150300"
   }
 });
