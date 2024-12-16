9cbdb3ee024544a55ac64fdc4405803d8763e625
g97iulio1609 <giulio97.leone@gmail.com>
2024-12-16 12:24:42 +0100

    push

--- .firebase/alphaness-322423/hosting/flutter_service_worker.js
+++ .firebase/alphaness-322423/hosting/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bca382acc4af4ba01a0c0a6a46f30e36",
+const RESOURCES = {"flutter_bootstrap.js": "12810d9e46af03aa3c5b67536857c7e4",
 "version.json": "81ab2345c2d2356a30f146453b4106f1",
 "splash/img/light-2x.png": "46ff390123b669a0bc71e47abe44a337",
 "splash/img/dark-4x.png": "67b0b78dc18815bfc04e2e8266d51bab",
@@ -13,9 +13,9 @@ const RESOURCES = {"flutter_bootstrap.js": "bca382acc4af4ba01a0c0a6a46f30e36",
 "splash/img/dark-2x.png": "46ff390123b669a0bc71e47abe44a337",
 "splash/img/dark-1x.png": "570df92f1a54826c03eeeb72354d00ca",
 "splash/img/light-1x.png": "570df92f1a54826c03eeeb72354d00ca",
-"index.html": "31712c5612220005f2529fe72b996d14",
-"/": "31712c5612220005f2529fe72b996d14",
-"main.dart.js": "ff76a16e5945daa073b72f5c1ef01743",
+"index.html": "a5fe77927ed26555e48a087034c2a940",
+"/": "a5fe77927ed26555e48a087034c2a940",
+"main.dart.js": "d80e4d5a39f385d21846807b5ffd9085",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "0a10bece31b4f2e6c17c278a56146e58",
 "icons/Icon-192.png": "ed9836b8089b20b8c555fce3e44456e5",
